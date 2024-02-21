a1c1f975e50af75915fd314f278244833012179c
Elvis <elvis.sylejmani@rapidimages.se>
2024-02-21 16:57:18 +0100

    initial set-up

---
+++ Models/SqlDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Signalify.Models;
+
+interface ISqlDatabaseSettings
+{
+    public string? ConnectionString { get; set; }
+}
+
+public class SqlDatabaseSettings : ISqlDatabaseSettings
+{
+    public string? ConnectionString { get; set; }
+
+    public SqlDatabaseSettings(IConfiguration configuration)
+    {
+        ConnectionString = configuration.GetValue<string>("Database:ConnectionString");
+    }
+}
